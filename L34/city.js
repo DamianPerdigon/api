f50d5cb1c5f54e3055a5ae5bf34a2179c25349cf
Damián Perdigón Álvarez <87827043+DamianPerdigon@users.noreply.github.com>
2023-08-22 21:23:41 +0100

    Update city.js

--- L34/city.js
+++ L34/city.js
@@ -1,40 +1,63 @@
-const url = "https://wft-geo-db.p.rapidapi.com/v1/geo/cities/Q34647";
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "c6bcaba155msh0142505f38d30b0p107d0ejsnbc82651a1d2a",
-    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
-  },
-};
+// Function to fetch information about a city by its name and country code
+async function fetchCityInfo(cityName, countryCode) {
+  // Construct the URL, including the city name and optionally the country code
+  /*The encodeURIComponent() function encodes a URI by replacing each instance 
+  of certain characters by one, two, three, or four escape sequences representing 
+  the UTF-8 encoding of the character*/
+  const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities?namePrefix=${encodeURIComponent(cityName)}${countryCode ? `&countryIds=${countryCode}` : ''}`;
+  
+  // HTTP request options including method and headers
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c6bcaba155msh0142505f38d30b0p107d0ejsnbc82651a1d2a",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
 
-// La palabra clave "await" solo se puede usar dentro de una función asíncrona, así que la colocamos dentro de una
-async function fetchCityInfo() {
   try {
+    // Fetching the data from the URL
     const response = await fetch(url, options);
-    const result = await response.json(); // Si la respuesta es JSON, podemos usar el método json() para parsearla
-    console.log(`Latitud: ${result.data.latitude}`);
-    console.log(`Longitud: ${result.data.longitude}`);
-    // Puedes acceder a otros datos de la respuesta aquí, según la estructura de la respuesta de la API
-    const temperature = temperatureInfo(result.data);
-    console.log(temperature)
-    updateDOM(result.data);
+    // Parsing the response to JSON
+    const result = await response.json();
+    // Taking the first city from the array
+    const cityData = result.data[0];
+    // Fetching additional city details like elevation
+    await fetchCityDetails(cityData);
+    // Fetching temperature using latitude and longitude
+    await temperatureInfo(cityData);
+    // Updating the DOM with the fetched data
+    updateDOM(cityData);
   } catch (error) {
+    // Error handling 
     console.error(error);
   }
 }
 
-// Function to update the DOM with the fetched city and weather information
-function updateDOM(data) {
-  const { population, elevationMeters, latitude, longitude, temperature } = data;
-  document.getElementById("population").innerText = population;
-  document.getElementById("elevation").innerText = elevationMeters;
-  document.getElementById("latitude").innerText = latitude;
-  document.getElementById("longitude").innerText = longitude;
-  document.getElementById("currentTemperature").innerText = temperature;
+// Function to fetch additional city details like elevation using wikiDataId(unique identifier)
+async function fetchCityDetails(data) {
+  // Constructing the URL using the wikiDataId of the city
+  const url = `https://wft-geo-db.p.rapidapi.com/v1/geo/cities/${data.wikiDataId}`;
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "c6bcaba155msh0142505f38d30b0p107d0ejsnbc82651a1d2a",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  // Fetching the data from the URL
+  const response = await fetch(url, options);
+  // Parsing the response to JSON
+  const result = await response.json();
+  // Adding the elevation data 
+  data.elevationMeters = result.data.elevationMeters;
 }
 
+// Function to fetch temperature based on latitude and longitude
 async function temperatureInfo(data) {
-  const url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=28.041638888&lat=-26.204361111`;
+  // Constructing the URL using the city's latitude and longitude
+  const url = `https://weatherbit-v1-mashape.p.rapidapi.com/current?lon=${data.longitude}&lat=${data.latitude}`;
   const options = {
     method: "GET",
     headers: {
@@ -44,12 +67,29 @@ async function temperatureInfo(data) {
   };
 
   try {
+    // Fetching the data from the URL
     const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result)
+    // Parsing the response to JSON
+    const result = await response.json();
+    // Adding the temperature data 
+    data.temperature = result.data[0].temp;
   } catch (error) {
+    // Error handling 
     console.error(error);
   }
 }
 
-fetchCityInfo();
+// Function to update the DOM with city and weather information
+function updateDOM(data) {
+  // Destructuring the needed data
+  const { population, elevationMeters, latitude, longitude, temperature } = data;
+  // Updating the corresponding HTML elements with the fetched data
+  document.getElementById("population").innerText = population;
+  document.getElementById("elevation").innerText = elevationMeters;
+  document.getElementById("latitude").innerText = latitude;
+  document.getElementById("longitude").innerText = longitude;
+  document.getElementById("currentTemperature").innerText = temperature;
+}
+
+// Initial call to fetch city information for Johannesburg
+fetchCityInfo("Johannesburg", "ZA");
